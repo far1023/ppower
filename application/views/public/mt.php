5f804d2ac5248e70118686e7f21781d5bfd46979
Fuad Agil <far1023@users.noreply.github.com>
2020-11-16 09:05:03 +0700

    maintenance page

---
+++ application/views/public/mt.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Under Maintenance</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Ppower">
+	<meta name="description" content="Sistem Pemilihan Jurusan Kuliah.">
+	<style type="text/css">
+		.container{display:table;}.container{font-family:"Open Sans",sans-serif;}html,.footer,.container,body{width:100%;}html,.container,body{height:100%;}#lego{padding-left:.052083333in;}body,html{margin-left:0;}.container{background:#fff;}[class~=content]{display:table-cell;}[class~=content]{font-size:16.5pt;}[class~=content]{text-align:center;}html,body{margin-bottom:0;}html,body{margin-right:0;}body,html{margin-top:0;}#lego{padding-bottom:.052083333in;}body,html{padding-left:0;}[class~=content]{vertical-align:middle;}#lego{padding-right:.052083333in;}html,body{padding-bottom:0;}html,body{padding-right:0;}[class~=content]{padding-left:1.875pc;}#lego{padding-top:.052083333in;}[class~=content]{padding-bottom:30pt;}[class~=content]{padding-right:1.875pc;}[class~=content]{padding-top:30pt;}[class~=inner-wrapper],[class~=svg-wrap]{display:inline-block;}body,html{padding-top:0;}[class~=main-title],[class~=top-title],a{color:#001f3f;}[class~=top-title]{font-size:2.1875pc;}[class~=top-title]{font-weight:700;}[class~=top-title]{margin-bottom:.260416667in;}[class~=main-title]{line-height:0;}[class~=main-title]{font-size:90px;}[class~=main-title]{font-weight:800;}[class~=svg-wrap]{font-size:0;}[class~=svg-wrap]{vertical-align:baseline;}[class~=blurb]{margin-left:0pc;}[class~=blurb]{margin-bottom:.3125in;}[class~=blurb]{margin-right:0pc;}[class~=blurb]{margin-top:0in;}[class~=blurb]{color:#7f7f7f;}[class~=blurb]{font-size:18px;}.footer{position:fixed;}.footer{left:0;}::-moz-selection{background:#b3d4fc;}.footer{bottom:37.5pt;}.footer{color:#707070;}.footer{font-size:x-small;}::selection{background:#b3d4fc;}.footer{text-align:center;}::selection{text-shadow:none;}a{text-decoration:none;}svg{vertical-align:middle;}::-moz-selection{text-shadow:none;}a:hover{opacity:.8;}@media only screen and (max-width: 440px){[class~=main-title]{font-size:.625in;}#lego{width:50px;}#lego{height:55px;}[class~=blurb]{font-size:.1875in;}#lego{padding-left:5px;}#lego{padding-bottom:7.5pt;}#lego{padding-right:5px;}#lego{padding-top:7.5pt;}}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+			<div class="inner-wrapper">
+				<div class="main-title">
+					<span class="svg-wrap"><img src="assets/images/illustration/surr-675.png" width="40%" alt="Maintenance"></span>
+				</div>
+			</div>			
+			<p class="blurb">
+				Terima kasih telah mengunjungi kami.<br />Mohon maaf saat ini sistem sedang dalam masa pengembangan.
+			</p>
+			<div class="footer">
+				from <a href="https://far1023.github.io/" title="Fuad Agil">Fuad Agil</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
